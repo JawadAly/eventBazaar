83c2528a911bf96934b83e171e0a9b43acf6122d
jawadSaleem <jawadsaleem932@gmail.com>
2024-08-22 16:20:41 +0500

    added pass addition functionality at step 4 of event onboarding

--- src/components/Step4.jsx
+++ src/components/Step4.jsx
@@ -1,47 +1,68 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import MUITextField from "../components/MUITextField";
 import SimpleMUIButton from "./SimpleMUIButton";
 import { stepContext } from "./EventsContext";
 import { CalendarIcon, ClockIcon, TagIcon } from "./Socials";
 import ModalWindow from "./MUIModelWindow";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import EditNoteIcon from '@mui/icons-material/EditNote';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditNoteIcon from "@mui/icons-material/EditNote";
 // import MUIModelWindow from "./MUIModelWindow";
 
 const Step4 = () => {
-  const[passDetails,setPassDetails] = useState({
-    passTitle : '',
-    passFullPrice : '',
-    passPrice : '',
-    passDiscount : '',
-    passExpiryDate : '',
+  const [passDetails, setPassDetails] = useState({
+    passTitle: "",
+    passFullPrice: "",
+    passPrice: "",
+    passDiscount: "",
+    passExpiryDate: "",
   });
-  const sensePassChange = (e) =>{
-    const{name,value} = e.target;
-    setPassDetails((prevVal)=>{
-      if(name === 'passPrice' && passDetails.passFullPrice !== ''){
-        const discountPercent = ((passDetails.passFullPrice - value)/passDetails.passFullPrice)*100;  
+  const [addOrUpdatePass, setAddOrUpdatePass] = useState(true);
+  const[updationKey,setUpdationKey] = useState('');
+  const sensePassChange = (e) => {
+    const { name, value } = e.target;
+    setPassDetails((prevVal) => {
+      if (name === "passPrice" && passDetails.passFullPrice !== "") {
+        const discountPercent =
+          ((passDetails.passFullPrice - value) / passDetails.passFullPrice) *
+          100;
         const roundedDiscount = Math.round(discountPercent);
-        return{
+        return {
           ...prevVal,
-          [name] : value,
-          passDiscount: roundedDiscount
+          [name]: value,
+          passDiscount: roundedDiscount,
         };
       }
-      return{
+      return {
         ...prevVal,
-        [name] : value
+        [name]: value,
       };
     });
-  }
-  const deletePass = (deletionKey) =>{
-    setEventData((prevVal)=>{
-      return{
+  };
+  const deletePass = (deletionKey) => {
+    setEventData((prevVal) => {
+      return {
         ...prevVal,
-        passDetails: prevVal.passDetails.filter((value,index)=> index !==deletionKey )
+        passDetails: prevVal.passDetails.filter(
+          (value, index) => index !== deletionKey
+        ),
       };
     });
-  }
+  };
+  
+  const updatePass = (updationKey) => {
+    // turning on updation btn in modal
+    setAddOrUpdatePass(false);
+    const toBeUpdatedPass = eventData.passDetails.find(
+      (value, index) => updationKey === index
+    );
+    console.log(toBeUpdatedPass);
+    setPassDetails(() => {
+      return toBeUpdatedPass;
+    });
+    document.getElementById('modalOpener').click();
+    // saving this updation key in a state for further updation process
+    setUpdationKey(updationKey);
+  };
   const {
     currentStep,
     setCurrentStep,
@@ -117,10 +138,20 @@ const Step4 = () => {
                 <h5>Pass Details</h5>
                 <button
                   id="modalOpener"
+                  className="d-none"
+                  type="button"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  Add Pass
+                </button>
+                <button
+                  // id="modalOpener"
                   className="themeColor addPassBtn"
                   type="button"
                   data-bs-toggle="modal"
                   data-bs-target="#exampleModal"
+                  onClick={()=>setAddOrUpdatePass(true)}
                 >
                   Add Pass
                 </button>
@@ -128,33 +159,35 @@ const Step4 = () => {
               <p className="ps-3 pe-3 text-center">
                 You need to add atleast one type of pass for paid events.
               </p>
-              {
-                eventData.passDetails.map((value,index)=>{
-                  return(
-                      <div className="eventPass p-3 mb-3" key={index}>
-                        <div className="d-flex align-items-center justify-content-between">
-                          <div className="passTitle">
-                            {value.passTitle}
-                          </div>
-                          <div className="actions">
-                            <EditNoteIcon className="themeColor me-2"/>
-                            <DeleteOutlineIcon className="themeColor" onClick={()=> deletePass(index)}/>
-                          </div>
-                        </div>
-                        <div className="passDetails d-flex mt-2">
-                          <p className="mb-0">
-                            <TagIcon font='small'/> {value.passPrice} 
-                          </p>
-                          <s className="percentage ms-3">{value.passFullPrice}</s>
-                          <p className="ms-3 mb-0">
-                            <ClockIcon font='small' incomingClass='themeColor' /> {value.passExpiryDate}
-                          </p>
-                        </div>
+              {eventData.passDetails.map((value, index) => {
+                return (
+                  <div className="eventPass p-3 mb-3" key={index}>
+                    <div className="d-flex align-items-center justify-content-between">
+                      <div className="passTitle">{value.passTitle}</div>
+                      <div className="actions">
+                        <EditNoteIcon
+                          className="themeColor me-2"
+                          onClick={() => updatePass(index)}
+                        />
+                        <DeleteOutlineIcon
+                          className="themeColor"
+                          onClick={() => deletePass(index)}
+                        />
                       </div>
-                  );
-                })
-              }
-
+                    </div>
+                    <div className="passDetails d-flex mt-2">
+                      <p className="mb-0">
+                        <TagIcon font="small" /> {value.passPrice}
+                      </p>
+                      <s className="percentage ms-3">{value.passFullPrice}</s>
+                      <p className="ms-3 mb-0">
+                        <ClockIcon font="small" incomingClass="themeColor" />{" "}
+                        {value.passExpiryDate}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <ModalWindow>
@@ -168,67 +201,99 @@ const Step4 = () => {
               ></button>
             </div>
             <div className="modal-body p-4 pe-5">
-              <MUITextField 
-              label='Pass Name' 
-              type='text'
-              name='passTitle'  
-              val={passDetails.passTitle}
-              changeEvent={sensePassChange}
+              <MUITextField
+                label="Pass Name"
+                type="text"
+                name="passTitle"
+                val={passDetails.passTitle}
+                changeEvent={sensePassChange}
               />
-              <MUITextField 
-              label='Full Price' 
-              type='number'
-              name='passFullPrice'  
-              val={passDetails.passFullPrice}
-              changeEvent={sensePassChange}
+              <MUITextField
+                label="Full Price"
+                type="number"
+                name="passFullPrice"
+                val={passDetails.passFullPrice}
+                changeEvent={sensePassChange}
               />
               <p className="p-2">Discount (Optional)</p>
               <div className="row">
                 <div className="col-md-6 col-6">
-                  <MUITextField 
-                  label='Price' 
-                  type='number'
-                  name='passPrice'  
-                  val={passDetails.passPrice}
-                  changeEvent={sensePassChange}
+                  <MUITextField
+                    label="Price"
+                    type="number"
+                    name="passPrice"
+                    val={passDetails.passPrice}
+                    changeEvent={sensePassChange}
                   />
                 </div>
                 <div className="col-md-6 col-6">
-                  <MUITextField 
-                  label='Percentage' 
-                  type='number'
-                  name='passDiscount'  
-                  val={passDetails.passDiscount}
-                  changeEvent={sensePassChange}
+                  <MUITextField
+                    label="Percentage"
+                    type="number"
+                    name="passDiscount"
+                    val={passDetails.passDiscount}
+                    changeEvent={sensePassChange}
                   />
                 </div>
               </div>
-              <MUITextField 
-              startAdornmentIcon={CalendarIcon}
-              label='Ending On' 
-              type='date'
-              name='passExpiryDate' 
-              val={passDetails.passExpiryDate}
-              changeEvent={sensePassChange}
+              <MUITextField
+                startAdornmentIcon={CalendarIcon}
+                label="Ending On"
+                type="date"
+                name="passExpiryDate"
+                val={passDetails.passExpiryDate}
+                changeEvent={sensePassChange}
               />
               <div className="d-flex align-items-center justify-content-around mt-3">
-                <button className="btn btn-secondary" type="button" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
-                <button 
-                className="btn btn-secondary"
-                type="button" 
-                style={{backgroundColor:'#bc2649'}}
-                onClick={()=>{
-                  setEventData((prevVal)=>{
-                    return{
-                      ...prevVal,
-                      passDetails:[...prevVal.passDetails,passDetails]
-                    };
-                  });
-                  alert('Pass added successfully');
-                }}
+                <button
+                  className="btn btn-secondary"
+                  type="button"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
                 >
-                Add Pass
+                  Cancel
                 </button>
+                {addOrUpdatePass ? (
+                  <button
+                    className="btn btn-secondary"
+                    type="button"
+                    style={{ backgroundColor: "#bc2649" }}
+                    onClick={() => {
+                      setEventData((prevVal) => {
+                        return {
+                          ...prevVal,
+                          passDetails: [...prevVal.passDetails, passDetails],
+                        };
+                      });
+                      alert("Pass added successfully");
+                    }}
+                  >
+                    Add Pass
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-secondary"
+                    type="button"
+                    style={{ backgroundColor: "#bc2649" }}
+                    onClick={(e) => {
+                      setEventData((prevVal)=>{
+                      const updatedPassArray = prevVal.passDetails.map((value,index)=>{
+                        if(index === updationKey){
+                            return passDetails;
+                        }
+                        return value;
+                      });
+                        return{
+                          ...prevVal,
+                          passDetails:updatedPassArray
+                        };
+                      });
+                      alert('Pass updated successfully!');
+                    }}
+                  >
+                    Update Pass
+                  </button>
+                )}
               </div>
             </div>
           </ModalWindow>
